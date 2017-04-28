b326a07ec2fa175a4004680c23e17c76d70a12c2
laxect <laxect39@gmail.com>
2017-04-28 22:48:45 +0800

    help updata

--- src/service/__help__.py
+++ src/service/__help__.py
@@ -4,6 +4,9 @@ str_help = (
     "\\exit to leave.\n"
     "\\tn to find which tunel you are now in.\n"
     "\\chtn + tunel_id to change your tunel.\n"
+    "\\chname + a_new_name to chang your name.\n"
+    "\\chtnname + a_new_name to chnage tunel's name\n"
+    "From laxect#laxect39@gmail.com"
 )
 
 
